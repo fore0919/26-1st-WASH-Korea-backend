bd7cd697fc9a7cbf8e6971e34ec53a67bf866b6a
권은경 <kwon-eunkyung@gwon-eungyeong-ui-MacBookAir.local>
2021-11-02 18:37:33 +0900

    ADD:models.py작성

---
+++ products/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+# Create your models here.
+
+class Category(models.Model):
+    name              = models.CharField(max_length=40)
+
+    class Meta:
+        db_table = 'categories'
+
+class SubCategory(models.Model):
+    name         = models.CharField(max_length=40)
+    category     = models.ForeignKey('Category',on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'sub_categories'
+
+class Product(models.Model):
+    name         = models.CharField(max_length=40)
+    price        = models.DecimalField(max_digits=8, decimal_places=2)
+    weight       = models.CharField(max_length=10)
+    sub_name     = models.CharField(max_length=40)
+    descriptio   = models.TextField()
+    sub_category = models.ForeignKey('SubCategory',on_delete=models.CASCADE)
+    tag          = models.ManyToManyField('Tag', through='ProductTag')
+    
+    class Meta:
+        db_table = 'products'
+
+class Tag(models.model):
+    name         = models.CharField(max_length=40)
+    
+    class Meta:
+        db_table = 'tags'
+
+class ProductTag(models.Model):
+    product      = models.ForeignKey('Product', on_delete=models.CASCADE)
+    tag          = models.ForeignKey('Tag', on_delete=models.CASCADE)
+   
+    class Meta:
+        db_table = 'product_tags'
+
+class Image(models.model):
+    url         = models.CharField(max_length=200)
+    product      = models.ForeignKey('Product', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'imege'
---
+++ users/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class User(models.Model):
+    user_id	       = models.CharField(max_length=40)
+    password  	   = models.CharField(max_length=200)
+    name      	   = models.CharField(max_length=40)
+    nickname       = models.CharField(max_length=40)
+    email     	   = models.CharField(max_length=100, unique=True)
+    contact   	   = models.CharField(max_length=15)
+    address		   = models.CharField(max_length=100)
+    created_at 	   = models.DateTimeField(auto_now_add=True)
+    updated_at 	   = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'users'
