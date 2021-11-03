9305df249852d4099c4745a2cba56aa54ec6c534
권은경 <kwon-eunkyung@gwon-eungyeong-ui-MacBookAir.local>
2021-11-03 11:20:38 +0900

    MODIFY:models.py 리뷰반영

--- products/models.py
+++ products/models.py
@@ -3,14 +3,14 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
-    name              = models.CharField(max_length=40)
+    name = models.CharField(max_length=40)
 
     class Meta:
         db_table = 'categories'
 
 class SubCategory(models.Model):
-    name         = models.CharField(max_length=40)
-    category     = models.ForeignKey('Category',on_delete=models.CASCADE)
+    name     = models.CharField(max_length=40)
+    category = models.ForeignKey('Category',on_delete=models.PROTECT)
 
     class Meta:
         db_table = 'sub_categories'
@@ -20,29 +20,29 @@ class Product(models.Model):
     price        = models.DecimalField(max_digits=8, decimal_places=2)
     weight       = models.CharField(max_length=10)
     sub_name     = models.CharField(max_length=40)
-    descriptio   = models.TextField()
+    description  = models.TextField()
     sub_category = models.ForeignKey('SubCategory',on_delete=models.CASCADE)
-    tag          = models.ManyToManyField('Tag', through='ProductTag')
+    tags         = models.ManyToManyField('Tag', through='ProductTag')
     
     class Meta:
         db_table = 'products'
 
 class Tag(models.Model):
-    name         = models.CharField(max_length=40)
+    name = models.CharField(max_length=40)
     
     class Meta:
         db_table = 'tags'
 
 class ProductTag(models.Model):
-    product      = models.ForeignKey('Product', on_delete=models.CASCADE)
-    tag          = models.ForeignKey('Tag', on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    tag     = models.ForeignKey('Tag', on_delete=models.CASCADE)
    
     class Meta:
-        db_table = 'product_tags'
+        db_table = 'products_tags'
 
-class Image(models.Model):
-    url         = models.CharField(max_length=200)
-    product      = models.ForeignKey('Product', on_delete=models.CASCADE)
+class ProducdtImage(models.Model):
+    url     = models.CharField(max_length=2000)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
 
     class Meta:
-        db_table = 'imege'
+        db_table = 'product_images'
--- users/models.py
+++ users/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
 class User(models.Model):
-    user_id	     = models.CharField(max_length=40)
-    password     = models.CharField(max_length=200)
-    name    	 = models.CharField(max_length=40)
-    nickname     = models.CharField(max_length=40,null=True)
-    email     	 = models.CharField(max_length=100, unique=True)
-    contact      = models.CharField(max_length=15)
-    address	     = models.CharField(max_length=100,null=True)
-    created_at 	 = models.DateTimeField(auto_now_add=True)
-    updated_at   = models.DateTimeField(auto_now=True)
+    user_name  = models.CharField(max_length=40, unique=True, default="")
+    password   = models.CharField(max_length=200)
+    name       = models.CharField(max_length=40)
+    nickname   = models.CharField(max_length=40, null=True)
+    email      = models.CharField(max_length=100, unique=True)
+    contact    = models.CharField(max_length=15)
+    address	   = models.CharField(max_length=100, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'users'
