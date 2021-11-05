a1161acb936575d8579964b786057b466a4a63c6
권은경 <kwon-eunkyung@gwon-eungyeong-ui-MacBookAir.local>
2021-11-05 15:38:32 +0900

    MODIFY:오타수정외 리뷰반영

--- users/views.py
+++ users/views.py
@@ -10,8 +10,8 @@ from .utils         import validate_email, validate_password
 
 class SignupView(View):
     def post(self, request):
-        data = json.loads(request.body)
         try :
+            data = json.loads(request.body)
             user_name = data['user_name']
             password  = data['password']
             name      = data['name']
@@ -21,11 +21,11 @@ class SignupView(View):
             address   = data['address']
 
             if not validate_email(email) :
-                return JsonResponse({'MASSAGE':'EMAIL_VALIDATION_ERROR'}, status=400)
+                return JsonResponse({'message':'EMAIL_VALIDATION_ERROR'}, status=400)
             if not validate_password(password):
-                return JsonResponse({'MASSAGE':'PW_VALIDATION_ERROR'}, status=400)
+                return JsonResponse({'message':'PW_VALIDATION_ERROR'}, status=400)
             if User.objects.filter(email=email).exists():
-                return JsonResponse({'MESSAGE':'ALREADY_EXISTS_EMAIL'}, status=409)
+                return JsonResponse({'message':'ALREADY_EXISTS_EMAIL'}, status=409)
 
             hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
@@ -38,29 +38,28 @@ class SignupView(View):
                 contact   = contact,
                 address   = address,
             )
-            return JsonResponse({'MASSAGE':'SUCCES'}, status=201)
+            return JsonResponse({'message':'SUCCESS'}, status=201)
 
         except KeyError:
-            return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
 
 class LoginView(View):
-    def post(self, request):
-        data = json.loads(request.body)
-        
+    def post(self, request):        
         try : 
+            data = json.loads(request.body)
             user_name = data['user_name']
             password  = data['password']
             user      = User.objects.get(user_name=user_name)
 
             if not bcrypt.checkpw(password.encode("utf-8"), user.password.encode('utf-8')):
-                return JsonResponse({'MESSAGE':'INVALID_USER'}, status=401)
+                return JsonResponse({'message':'INVALID_USER'}, status=401)
 
             access_token = jwt.encode({'id':user.id},settings.SECRET_KEY, algorithm = settings.ALGORITHM)
 
-            return JsonResponse({'MESSAGE':'SUCCESS', 'ACCESS_TOKEN': access_token}, status=200)
+            return JsonResponse({'message':'SUCCESS', 'ACCESS_TOKEN': access_token}, status=200)
 
         except KeyError:
-            return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
+            return JsonResponse({'message':'KEY_ERROR'}, status=400)
 
         except User.DoesNotExist:
-            return JsonResponse({'MASSAGE':'USER_DOES_NOT_EXISTS'}, status=401)
+            return JsonResponse({'message':'USER_DOES_NOT_EXISTS'}, status=401)
