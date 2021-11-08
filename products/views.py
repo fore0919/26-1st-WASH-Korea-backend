c57ed333931c45bd1b692e718e67d566bda5db23
강경훈 <kyunghun@gang-gyeonghuns-MacBook-Pro.local>
2021-11-08 18:10:41 +0900

    mentor review

--- products/views.py
+++ products/views.py
@@ -8,9 +8,12 @@ from products.models import Product, SubCategory
 
 class ProductListView(View):
     def get(self, request):
-        category     = request.GET['category']
-        sub_category = request.GET.get('sub_category')
-        price        = request.GET.get('price')
+        DEFAULT_CATEGORY_ID = 1
+    
+        data         = json.loads(request.body)
+        category     = data.get('category', DEFAULT_CATEGORY_ID)
+        sub_category = data.get('sub_category')
+        sorting      = data.get('sort', 'id')
 
         q = Q()
 
@@ -20,10 +23,11 @@ class ProductListView(View):
         if sub_category:
             q &= Q(sub_category_id = sub_category)
 
-        product_list = Product.objects.filter(q)
-
-        if price:
-            product_list = product_list.order_by(price)
+        sort = {
+            "price"  : "price",
+            "-price" : "-price",
+            "id"     : "id"
+        }
 
         results = [{
                     "name"          : product.name,
@@ -31,11 +35,11 @@ class ProductListView(View):
                     "sub_name"      : product.sub_name,
                     'tags'          : [tag.name for tag in product.tags.all()],
                     "product_image" : [image.url for image in product.productimage_set.all()]
-            } for product in product_list]
+            } for product in Product.objects.filter(q).order_by(sort[sorting])]
 
         return JsonResponse({'results' : results}, status = 200)
 
-class NavListView(View):
+class CategoryView(View):
     def get(self, request):
         category     = request.GET.get('category')
         sub_category = request.GET.get('sub_category')
@@ -48,9 +52,9 @@ class NavListView(View):
         if sub_category:
             q &= Q(id = sub_category)
 
-        sub_category_list = SubCategory.objects.filter(q)
-
-        results = [{
+        sub_category = SubCategory.objects.filter(q)
+        results = {
+                    "category"
                     "sub_category"             : sub_category.name,
                     "sub_category_image"       : sub_category.image,
                     "sub_category_description" : sub_category.description,
@@ -58,7 +62,7 @@ class NavListView(View):
                     "category_image"           : sub_category.category.image,
                     "category_description"     : sub_category.category.description,
                     "account"                  : len(Product.objects.filter(sub_category_id = sub_category))
-            } for sub_category in sub_category_list]
+            }
 
         return JsonResponse({'results' : results}, status = 200)
 
@@ -66,10 +70,13 @@ class SearchView(View):
     def get(self, request):
         search_word = request.GET.get('search_word')
 
-        q = Q()
-
         if search_word:
-            q = Product.objects.filter(Q(sub_category__category__name__startswith = search_word)|Q(sub_category__name__startswith = search_word)|Q(name__startswith = search_word)|Q(tags__name__startswith = search_word))
+            products = Product.objects.filter(
+                Q(sub_category__category__name__startswith = search_word)|
+                Q(sub_category__name__startswith = search_word)|
+                Q(name__startswith = search_word)|
+                Q(tags__name__startswith = search_word)
+            )
 
         results = [{
                     "name"          : product.name,
@@ -77,6 +84,6 @@ class SearchView(View):
                     "sub_name"      : product.sub_name,
                     'tags'          : [tag.name for tag in product.tags.all()],
                     "product_image" : [image.url for image in product.productimage_set.all()]
-            } for product in q]
+            } for product in products]
 
         return JsonResponse({'results' : results}, status = 200)
